30ea125a02afc56ce84125824beae23cb6464b75
Fabien Ryckoort <fabien.ryckoort@adeo.com>
2020-07-23 14:48:25 +0200

    doc

--- src/components/Redocusaurus/index.jsx
+++ src/components/Redocusaurus/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import ExecutionEnvironment from '@docusaurus/ExecutionEnvironment';
 import merge from 'lodash.merge';
 import useThemeContext from '@theme/hooks/useThemeContext';
 import './style.css';
